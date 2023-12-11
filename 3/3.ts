5b3ceec58aeb250e675c4ebdd3cf126c4aa00e9f
tulio <lenientmp99@gmail.com>
2023-12-11 02:27:49 -0300

    feat: day 3

--- 3/3.ts
+++ 3/3.ts
@@ -1,40 +1,138 @@
 import { readInput, splitLines } from '../utils';
 
-const isNumeric = (str: string) => {
-  return /^\d+$/.test(str);
-};
+const isNumeric = (str: string) => /^\d+$/.test(str);
+const isSymbol = (value: string): boolean =>
+  !isNumeric(value) && value !== '.' && value !== '\n';
 
-const checkAdjacent = (position: number, lineLenght: number) => {
-  const topline = [
-    position - lineLenght - 1,
-    position - lineLenght,
-    position - lineLenght + 1,
-  ];
+const starMap = new Map<number, number>();
 
-  const middleLine = [position - 1, position + 1];
+const checkAdjacent = (
+  valueRange: number[],
+  lineLenght: number,
+  input: string
+) => {
+  for (let position of valueRange) {
+    const topline =
+      position > lineLenght
+        ? [
+            position - lineLenght,
+            position - lineLenght - 1,
+            position - lineLenght - 2,
+          ]
+        : [];
 
-  const bottomLine = [
-    position + lineLenght - 1,
-    position + lineLenght,
-    position + lineLenght + 1,
-  ];
+    const middleLine = [position - 1, position + 1];
 
-  const adjacentPositions = [...topline, ...middleLine, ...bottomLine];
-  console.log(adjacentPositions);
-};
+    const bottomLine =
+      position < input.length - lineLenght
+        ? [
+            position + lineLenght,
+            position + lineLenght + 1,
+            position + lineLenght + 2,
+          ]
+        : [];
 
-const firstStep = () => {
-  const input = readInput('./input');
-  const lineLenght = input.indexOf('\n');
-  const result = 0;
-  const isSymbol = (i: number): boolean =>
-    !isNumeric(input[i]) && input[i] !== '.';
+    const adjacentPositions = [...topline, ...middleLine, ...bottomLine];
 
-  for (let i = 0; i < input.length; i++) {
-    if (isSymbol(i)) {
-      checkAdjacent(i, lineLenght);
+    for (let adjacentIndex of adjacentPositions) {
+      if (isSymbol(input[adjacentIndex])) return true;
     }
   }
+  return false;
 };
 
-console.log(firstStep());
+const checkAdjacent2 = (
+  valueRange: number[],
+  lineLenght: number,
+  input: string
+): number => {
+  for (let position of valueRange) {
+    const topline =
+      position > lineLenght
+        ? [
+            position - lineLenght,
+            position - lineLenght - 1,
+            position - lineLenght - 2,
+          ]
+        : [];
+
+    const middleLine = [position - 1, position + 1];
+
+    const bottomLine =
+      position < input.length - lineLenght
+        ? [
+            position + lineLenght,
+            position + lineLenght + 1,
+            position + lineLenght + 2,
+          ]
+        : [];
+
+    const adjacentPositions = [...topline, ...middleLine, ...bottomLine];
+    const currentValue = Number(valueRange.map((v) => input[v]).join(''));
+
+    for (let adjacentIndex of adjacentPositions) {
+      if (input[adjacentIndex] !== '*') continue;
+
+      if (starMap.get(adjacentIndex)) {
+        let starAdjacentProduct = currentValue * starMap.get(adjacentIndex)!;
+        starMap.delete(adjacentIndex);
+        return starAdjacentProduct;
+      }
+
+      starMap.set(adjacentIndex, currentValue);
+      return 0;
+    }
+  }
+  return 0;
+};
+
+const firstPart = () => {
+  const input = readInput('3/input');
+  const lineLenght = input.indexOf('\n');
+  let result = 0;
+
+  for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+    if (isNumeric(input[inputIndex])) {
+      let valueRange = [];
+      let valueIndex = inputIndex;
+
+      while (isNumeric(input[valueIndex])) {
+        valueRange.push(valueIndex);
+        valueIndex++;
+      }
+
+      if (checkAdjacent(valueRange, lineLenght, input)) {
+        result += Number(valueRange.map((v) => input[v]).join(''));
+      }
+      inputIndex = valueIndex;
+    }
+  }
+
+  return result;
+};
+
+const secondPart = () => {
+  const input = readInput('3/input');
+  const lineLenght = input.indexOf('\n');
+  let result = 0;
+
+  for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+    if (isNumeric(input[inputIndex])) {
+      let valueRange = [];
+      let valueIndex = inputIndex;
+
+      while (isNumeric(input[valueIndex])) {
+        valueRange.push(valueIndex);
+        valueIndex++;
+      }
+
+      result += checkAdjacent2(valueRange, lineLenght, input);
+      inputIndex = valueIndex;
+    }
+  }
+
+  return result;
+};
+
+console.log(firstPart());
+console.log(secondPart());
