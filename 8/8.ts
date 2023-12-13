ef77a378020255139c55cd598737540eadac7f24
tulio <lenientmp99@gmail.com>
2023-12-13 15:41:50 -0300

    feat: day 8 part 2

--- 8/8.ts
+++ 8/8.ts
@@ -1,4 +1,23 @@
 import { readInput } from '../utils';
+
+const lcm = (numbers: number[]) => {
+  let result = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    result = (result * numbers[i]) / gcd(result, numbers[i]);
+  }
+
+  return result;
+};
+
+const gcd = (a: number, b: number): number => {
+  if (b == 0) {
+    return a;
+  }
+
+  return gcd(b, a % b);
+};
+
 const secondPart = () => {
   const input = readInput('8/input');
   const [stepsList, directionsList] = input.trim().split('\n\n');
@@ -17,6 +36,55 @@ const secondPart = () => {
       ];
     })
   );
+
+  let startPositions = Array.from(directionsMap.keys()).filter((k) =>
+    k.endsWith('A')
+  );
+
+  let totalCycles = [];
+
+  for (let startPostion of startPositions) {
+    let currentPosition = startPostion;
+    let stepIndex = 0;
+    let currentCycles = [];
+    let firstZPosition = null;
+    let stepsCount = 0;
+
+    while (true) {
+      while (!currentPosition.endsWith('Z') || stepsCount == 0) {
+        let nextStep = stepsArray[stepIndex];
+        let nextPositionArray = directionsMap.get(currentPosition);
+
+        currentPosition =
+          nextStep == 'L' ? nextPositionArray![0] : nextPositionArray![1];
+
+        stepsCount = stepsCount + 1;
+
+        if (stepIndex == stepsArray.length - 1) {
+          stepIndex = 0;
+          continue;
+        }
+
+        stepIndex = stepIndex + 1;
+      }
+
+      currentCycles.push(stepsCount);
+
+      if (firstZPosition == null) {
+        firstZPosition = currentPosition;
+        stepsCount = 0;
+        continue;
+      }
+
+      if (currentPosition == firstZPosition) {
+        break;
+      }
+    }
+
+    totalCycles.push(currentCycles);
+  }
+
+  return lcm(totalCycles.flat());
 };
 
 const firstPart = () => {
@@ -44,7 +112,7 @@ const firstPart = () => {
 
   while (currentStep != 'ZZZ') {
     let nextStep = stepsArray[stepIndex];
-    let nextPositionArray = directionsMap.get(currentStep.trim());
+    let nextPositionArray = directionsMap.get(currentStep);
 
     currentStep =
       nextStep == 'L' ? nextPositionArray![0] : nextPositionArray![1];
