9cdaed9c96885e54a929650436d1d5bc2a373bc4
tulio <lenientmp99@gmail.com>
2023-12-18 17:42:13 -0300

    feat: day 13 part 2

--- 13/13.ts
+++ 13/13.ts
@@ -1,39 +1,22 @@
 import { readInput } from '../utils';
 
-// const findReflection = (
-//   mirror: string
-// ): { count: number; rowsAbove: number } => {
-//   const lines = mirror.split('\n');
-//   let count = 0;
-//   for (let i = 0; i < lines.length; i++) {
-//     if (lines[i] == lines[i + 1]) {
-//       let upper = i;
-//       let lower = i + 1;
-//       while (lines[upper] == lines[lower]) {
-//         count += 1;
-//         upper -= 1;
-//         lower += 1;
-//       }
-//       if (count > 1)
-//         return {
-//           count: count,
-//           rowsAbove: i + 1,
-//         };
-//       count = 0;
-//     }
-//   }
-//   return { count: 0, rowsAbove: 0 };
-// };
+const compareWithSmudge = (row1: string, row2: string): boolean => {
+  let smudgeCount = 0;
+  for (let i = 0; i < row1.length; i++) {
+    if (row1[i] != row2[i]) smudgeCount += 1;
+  }
+  return smudgeCount == 1;
+};
 
 const findReflection = (mirror: string): number => {
-  const lines = mirror.split('\n');
-  for (let i = 0; i < lines.length; i++) {
-    if (lines[i] == lines[i + 1]) {
+  const rows = mirror.split('\n');
+  for (let i = 0; i < rows.length; i++) {
+    if (rows[i] == rows[i + 1]) {
       let upper = i;
       let lower = i + 1;
 
-      while (lines[upper] == lines[lower]) {
-        if (upper == 0 || lower == lines.length - 1) return i + 1;
+      while (rows[upper] == rows[lower]) {
+        if (upper == 0 || lower == rows.length - 1) return i + 1;
         upper -= 1;
         lower += 1;
       }
@@ -42,12 +25,60 @@ const findReflection = (mirror: string): number => {
   return 0;
 };
 
-const rotateMirror = (mirror: string): string => {
+const findReflection2 = (mirror: string): number => {
+  const rows = mirror.split('\n');
+  for (let i = 0; i < rows.length; i++) {
+    if (i == rows.length - 1) return 0;
+
+    if (rows[i] == rows[i + 1]) {
+      let upper = i;
+      let lower = i + 1;
+      let smudgeCount = 0;
+
+      while (true) {
+        if (smudgeCount == 1 && (upper == 0 || lower == rows.length - 1))
+          return i + 1;
+
+        if (upper == 0 || lower == rows.length - 1) break;
+
+        upper -= 1;
+        lower += 1;
+
+        if (rows[upper] == rows[lower]) {
+          continue;
+        }
+
+        if (compareWithSmudge(rows[upper], rows[lower])) {
+          smudgeCount += 1;
+          continue;
+        }
+
+        break;
+      }
+    }
+
+    if (compareWithSmudge(rows[i], rows[i + 1])) {
+      if (i == 0 || i + 1 == rows.length - 1) return i + 1;
+
+      let upper = i - 1;
+      let lower = i + 2;
+
+      while (rows[upper] == rows[lower]) {
+        if (upper == 0 || lower == rows.length - 1) return i + 1;
+        upper -= 1;
+        lower += 1;
+      }
+    }
+  }
+  return 0;
+};
+
+const rotateMatrix = (mirror: string): string => {
   const matrix = mirror.split('\n').map((el) => el.split(''));
   const rotatedMatrix = matrix[0].map((_, colIndex) =>
     matrix.map((row) => row[colIndex])
   );
-  return rotatedMatrix.map((row) => row.join('n')).join('\n');
+  return rotatedMatrix.map((row) => row.join('')).join('\n');
 };
 
 const firstPart = () => {
@@ -58,10 +89,11 @@ const firstPart = () => {
     let horizontal = findReflection(mirror);
     if (horizontal) {
       result += horizontal * 100;
+      continue;
     }
 
-    let rotatedMirror = rotateMirror(mirror);
-    let veritcal = findReflection(rotatedMirror);
+    let rotatedMatrix = rotateMatrix(mirror);
+    let veritcal = findReflection(rotatedMatrix);
 
     result += veritcal;
   }
@@ -70,17 +102,19 @@ const firstPart = () => {
 };
 
 const secondPart = () => {
-  const mirrors = readInput('13/input').trim().split('\n\n');
+  const input = readInput('13/input').trim().split('\n\n');
   let result = 0;
 
-  for (let mirror of mirrors) {
-    let horizontal = findReflection(mirror);
+  for (let i = 0; i < input.length; i++) {
+    let matrix = input[i];
+    let horizontal = findReflection2(matrix);
     if (horizontal) {
       result += horizontal * 100;
+      continue;
     }
 
-    let rotatedMirror = rotateMirror(mirror);
-    let veritcal = findReflection(rotatedMirror);
+    let rotatedMatrix = rotateMatrix(matrix);
+    let veritcal = findReflection2(rotatedMatrix);
 
     result += veritcal;
   }
